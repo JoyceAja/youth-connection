38f7d4619a553ba599fc9722a7fc1edcfcaf34c7
JoyceAja <ajagbe.joyce@gmail.com>
2018-09-19 06:51:53 -0400

    working with type checking

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-type";
 
-import "./Layout.css"
+import "./Layout.css";
 
-export default class Jobs extends React.Component {
+class Layout extends React.Component {
   constructor(props) {
     super(props);
 
@@ -20,13 +20,13 @@ export default class Jobs extends React.Component {
           {this.props.dataArr.map((el, idx) => (
             <div className="job-box box">
               <div className="sub-header">Agency</div>
-                <div className="job-name title-header">{el.agency} </div>
-                <div className="job-address job">{`${el.address}, ${el.borough_community}`}</div>
-                <div className="job-contact job">{`${el.contact_number}`}</div>
-                <div className="sub-header">Employment Site</div>
-                <div className="job-site title-header">{`${el.site_name}`}</div>
-                <div className="job-program job">{`${el.program}`}</div>
-              </div>
+              <div className="job-name title-header">{el.agency} </div>
+              <div className="job-address job">{el.address? `${el.address}, ${el.borough_community}`: el.borough_community}</div>
+              <div className="job-contact job">{`${el.contact_number}`}</div>
+              <div className="sub-header">Employment Site</div>
+              <div className="job-site title-header">{`${el.site_name}`}</div>
+              <div className="job-program job">{`${el.program}`}</div>
+            </div>
           ))}
         </div>
       </div>
@@ -34,7 +34,17 @@ export default class Jobs extends React.Component {
   }
 }
 
-Jobs.propTypes = {
-  children: PropTypes.element.isRequired,
-  dataArr: PropTypes.array
-}
+// Layout.propTypes = {
+//   dataArr: PropTypes.array(
+//     PropTypes.shape({
+//       agency: PropTypes.string.isRequired,
+//       address: PropTypes.string.isRequired,
+//       borough_community: PropTypes.string.isRequired,
+//       contact_number: PropTypes.number.isRequired,
+//       site_name: PropTypes.string.isRequired,
+//       program: PropTypes.string.isRequired
+//     }).isRequired
+//   ).isRequired
+// };
+
+export default Layout;
